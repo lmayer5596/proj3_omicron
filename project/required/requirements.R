0d6e91cf7b6c1c659b6e08315e18b989ac783bfe
Lily Mayer <150291424+lmayer5596@users.noreply.github.com>
2024-02-27 14:09:49 -0500

    create first model

--- project/required/requirements.R
+++ project/required/requirements.R
@@ -1,4 +1,7 @@
 install.packages('data.table')
-
+install.packages('glmnet')
 
 library(data.table)
+library(caret)
+library(Metrics)
+library(glmnet)
--- project/src/models/train_model.R
+++ project/src/models/train_model.R
@@ -1,8 +1,9 @@
 #takes the edited datasets from the interim folder
-test <- fread('./project/volume/data/interim/test.csv')[, !c('dose_2', 'dose_3')]
-train <- fread('./project/volume/data/interim/train.csv')[, !c('dose_2', 'dose_3')]
+test <- fread('./project/volume/data/interim/test.csv')
+train <- fread('./project/volume/data/interim/train.csv')
 submit <- fread('./project/volume/data/interim/submit.csv')
 View(train)
+
 #saves training result column
 train_y <- train$ic50_Omicron
 
@@ -11,10 +12,36 @@ dummies <- dummyVars(ic50_Omicron ~ ., data = train)
 train <- predict(dummies, newdata = train)
 test <- predict(dummies, newdata = test)
 
+test <- data.table(test)
+train <- data.table(train)
+
+#cross validation
+train <- as.matrix(train)
+gl_model <- cv.glmnet(train, train_y, alpha = 1,family = "gaussian")
+bestlam <- gl_model$lambda.min
+
+gl_model <- glmnet(train, train_y, alpha = 1,family="gaussian")
+
+saveRDS(gl_model,"./project/volume/models/gl_model.model")
+
+test$ic50_Omicron <- 0
+test <- as.matrix(test)
+
+#use model
+pred <- predict(gl_model, s = bestlam, newx = test)
+predict(gl_model, s = bestlam, newx = test, type = "coefficients")
+
+#puts the final result into the submission file format
+submit$ic50_Omicron <- pred
+
+#saves the final submission as a csv
+fwrite(submit, './project/volume/data/processed/submit.csv')
+
 #reformats the output into a data.table
 train <- data.table(train)
 train$ic50_Omicron <- train_y
 test <- data.table(test)
+
 
 #uses a logistic model to fit the train data
 glm_model <- glm(ic50_Omicron~., data = train)
