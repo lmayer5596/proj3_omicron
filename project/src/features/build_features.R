90bf126471b228b3da29251f9263d2fc5f436daa
Lily Mayer <150291424+lmayer5596@users.noreply.github.com>
2024-03-03 12:04:58 -0500

    submission attempts

--- project/src/features/build_features.R
+++ project/src/features/build_features.R
@@ -17,7 +17,7 @@ master$sort_col <- 1:nrow(master)
 master[master == ''] <- NA
 
 #turns NAs in categorical variables into 'none' or 0
-master$dose_3 <- 1 * (!is.na(master$dose_3))
+#master$dose_3 <- 1 * (!is.na(master$dose_3))
 master[is.na(master$priorSxAtFirstVisit)]$priorSxAtFirstVisit <- 'N'
 master$priorSxAtFirstVisit <- 1 * (master$priorSxAtFirstVisit == 'Y')
 master$Sx_severity_most_recent[is.na(master$Sx_severity_most_recent)] <- 0
@@ -27,14 +27,15 @@ master[priorSxAtFirstVisitSeverity == 'Mild', priorSxAtFirstVisitSeverity := 1]
 master[priorSxAtFirstVisitSeverity == 'Moderate', priorSxAtFirstVisitSeverity := 2]
 master[priorSxAtFirstVisitSeverity == 'Severe', priorSxAtFirstVisitSeverity := 3]
 
-#master[train == 1, .(avg_omc = mean(ic50_Omicron, na.rm = TRUE)), by = posTest_beforeVisit]
-master$posTest_beforeVisit <- 1 * (master$posTest_beforeVisit == 'Yes')
-#master[posTest_beforeVisit == 'No', posTest_beforeVisit := 0]
-#master[posTest_beforeVisit == 'Yes', posTest_beforeVisit := 1]
-#master$posTest_beforeVisit <- 1*(master$posTest_beforeVisit == 'Yes')
+master$dose_3[is.na(master$dose_3)] <- 0
+master[dose_3 == 'BNT162b2', dose_3 := 1]
+master[dose_3 == 'mRNA1272', dose_3 := 2]
+master[dose_3 == 'AZD1222', dose_3 := 3]
 
-#master[priorSxAtFirstVisit == 'N', priorSxAtFirstVisit := 0]
-#master[priorSxAtFirstVisit == 'Y', priorSxAtFirstVisit := 1]
+master$posTest_beforeVisit <- 1 * (master$posTest_beforeVisit == 'Yes')
+master$sex <- 1 * (master$sex == 'Female')
+master$centre <- 1 * (master$centre == 'crick')
+master$dose_2 <- 1 * (master$dose_2 == 'BNT162b2')
 
 #removes negative numbers and zeroes and turns them into a larger number at the upper end of the range
 master$days_sinceDose2[is.na(master$days_sinceDose2)] <- 0
@@ -51,9 +52,6 @@ master[days_sinceSxLatest <= 0, days_sinceSxLatest := max(master$days_sinceSxLat
 
 master$days_sincePosTest_latest[is.na(master$days_sincePosTest_latest)] <- 0
 master[days_sincePosTest_latest <= 0, days_sincePosTest_latest := max(master$days_sincePosTest_latest)]
-#View(master)
-#splits data back into test and train
-#master <- master[, !c('posTest_beforeVisit', 'priorSxAtFirstVisit')]
 
 #maintains order so rows do not shuffle
 setkey(master, sample_id)
@@ -70,13 +68,7 @@ test <- test[, !c('sample_id', 'train', 'sort_col')]
 
 format$ic50_Omicron <- 0
 
-#treat negative values as NA- turn them into NA then treat all NA as the same
-#there is a window of a too small data or a too big date which wouldn't affect anything
-#try 21 for days since dose 3
-#add None for missing values in a character column
-
-#after main data is clean- merge with covar then use lecture 9
-
+View(master)
 #moves edited data into the interim folder
 fwrite(train, './project/volume/data/interim/train.csv')
 fwrite(test, './project/volume/data/interim/test.csv')
