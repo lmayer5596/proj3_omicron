8680258a7d13f98d742b8a10fb6263b5b79f60ec
Lily Mayer <150291424+lmayer5596@users.noreply.github.com>
2024-03-20 13:04:42 -0400

    submission attempts

---
+++ project/src/features/build_features_XGB.R
@@ -0,0 +1,47 @@
+#reads in all raw data sets
+test <- fread('./project/volume/data/raw/Stat_380_test.csv')
+train <- fread('./project/volume/data/raw/Stat_380_train.csv')
+covar <- fread('./project/volume/data/raw/covar_data.csv')
+format <- fread('./project/volume/data/raw/Example_sub.csv')
+
+#merges all data together into one data table
+test$ic50_Omicron <- 0
+test$train <- 0
+train$train <- 1
+master <- rbind(test, train)
+
+#creates column to save row order
+master$sort_col <- 1:nrow(master)
+
+#turns all blanks into NA
+master[master == ''] <- NA
+
+#changes dose_3 to numeric
+master$dose_3[!is.na(master$dose_3)] <- 1
+master$dose_3[is.na(master$dose_3)] <- 0
+#master[dose_3 == 'BNT162b2', dose_3 := 1]
+#master[dose_3 == 'mRNA1272', dose_3 := 2]
+#master[dose_3 == 'AZD1222', dose_3 := 3]
+
+#maintains order so rows do not shuffle
+setkey(master, sample_id)
+setkey(covar, sample_id)
+
+#merges unnamed data
+master <- merge(master, covar, all.x = TRUE)
+
+#separates back into train and test
+train <- master[train == 1][order(sort_col)]
+test <- master[train == 0][order(sort_col)]
+
+#removes unwanted columns
+train <- train[, !c('sample_id', 'train', 'sort_col')]#, 'dose_2', 'dose_3')]#, 'days_sinceDose2', 'days_sinceDose3', 'days_sincePosTest_latest')]
+test <- test[, !c('sample_id', 'train', 'sort_col')]#, 'dose_2', 'dose_3')]#, 'days_sinceDose2', 'days_sinceDose3', 'days_sincePosTest_latest')]
+
+format$ic50_Omicron <- 0
+
+#moves edited data into the interim folder
+fwrite(train, './project/volume/data/interim/train.csv')
+fwrite(test, './project/volume/data/interim/test.csv')
+fwrite(format, './project/volume/data/interim/submit.csv')
+
--- project/src/models/train_model_XGB.R
+++ project/src/models/train_model_XGB.R
@@ -1,13 +1,11 @@
 test <- fread('./project/volume/data/interim/test.csv')
 train <- fread('./project/volume/data/interim/train.csv')
 submit <- fread('./project/volume/data/interim/submit.csv')
+View(train)
 
 #save data before loss
 y.train <- train$ic50_Omicron
 y.test <- test$ic50_Omicron
-
-#train <- train[, c('age', 'sex', 'centre')]
-#test <- test[, c('age', 'sex', 'centre')]
 
 #using dummy variables to predict
 dummies <- dummyVars(ic50_Omicron~ ., data = train)
@@ -17,14 +15,14 @@ x.test <- predict(dummies, newdata = test)
 dtrain <- xgb.DMatrix(x.train, label = y.train, missing = NA)
 dtest <- xgb.DMatrix(x.test, missing = NA)
 
-#hyper_perm_tune <- NULL
+#hyper_perm_tune2 <- NULL
 
 #cross validation
 param <- list(  objective           = "reg:linear",
                 booster             = "gbtree",
                 eval_metric         = "rmse",
                 tree_method         = 'hist',
-                eta                 = 0.05,
+                eta                 = 0.005,
                 max_depth           = 20, #depth- move up from best value
                 min_child_weight    = 1, #depth
                 gamma               = 0.00, #depth
@@ -42,14 +40,15 @@ new_row$best_ntrees <- best_ntrees
 
 test_error <- unclass(XGBm)$evaluation_log[best_ntrees, ]$test_rmse_mean
 new_row$test_error <- test_error
-hyper_perm_tune <- rbind(new_row, hyper_perm_tune)
-hyper_perm_tune
+hyper_perm_tune2 <- rbind(new_row, hyper_perm_tune2)
+hyper_perm_tune2
 
 #use model
+best_ntrees = 180
 watchlist <- list(train = dtrain)
 XGBm <- xgb.train(params = param, nrounds = best_ntrees, missing = NA, data = dtrain, watchlist = watchlist, print_every_n = 1)
 pred <- predict(XGBm, newdata = dtest)
 
 submit$ic50_Omicron <- pred
 View(submit)
-fwrite(submit, './project/volume/data/processed/submit11.csv')
+fwrite(submit, './project/volume/data/processed/submit12.csv')
